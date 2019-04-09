0d9157b094a6a466ca55d61e1e7090d054a1d702
americanman <snowboardracer7@gmail.com>
2019-04-08 21:13:05 -0400

    Add example how it works

--- index.js
+++ index.js
@@ -9,3 +9,25 @@ const add = (x) => {
 }
 
 console.log(add(10)(20)(30))
+
+// Currying and Partial Function Application
+
+const volume = (l) => {
+  return (w, h) => {
+    return l * w * h
+  }
+}
+
+console.log(volume(70)(203, 142))
+
+// also can be called in this way
+// because hCy is just a declaration of anonimous function
+// that returned by volume
+// it is just (w, h) => {
+//   return l * w * h
+// }
+// it works becouse l comes from cloasure
+
+const hCy = volume(70)
+
+console.log(hCy(203, 142))
